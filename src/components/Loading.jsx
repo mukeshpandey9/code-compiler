9b7c3e7b5f1da452511a797633ebc3badac88be1
Mukesh Pandey <106433522+mukeshpandey9@users.noreply.github.com>
2023-11-13 22:08:27 +0530

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -7,7 +7,7 @@ function Loading() {
       <Watch
         height="80"
         width="80"
-        radius="48"
+        radius="50"
         color="#4fa94d"
         ariaLabel="watch-loading"
         wrapperStyle={{}}
