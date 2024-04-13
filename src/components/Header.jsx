3622819806c3452a6c5a6a322c7b5d3dbcc4dbe8
Mukesh Pandey <106433522+mukeshpandey9@users.noreply.github.com>
2024-04-13 15:43:24 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <nav className="bg-slate-950 text-lg text-white font-semibold">
       <div className="flex container px-4 md:px-16 h-16  justify-between items-center  mx-auto ">
         <div className="logo text-3xl font-bold text-slate-400">Code Hell</div>
-        <h3 className="text-center font-semibold text-green-400">~ By Mukesh Pandey</h3>
+        <h3 className="text-center font-semibold text-green-400">~ By Mukesh Pandey.</h3>
 
         <div className="menu">
           <ul className="md:flex gap-10 hidden">
